506f321ec9afa3de52a9bc9367f8791807eb6528
Umid <111351596+vSunShineJk@users.noreply.github.com>
2022-11-05 21:14:47 +0500

    05.11.2022

--- src/test/java/com/runner/CukesRunner.java
+++ src/test/java/com/runner/CukesRunner.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {
+                "pretty",
                 "html:target/cucumber-report.html",
                 "rerun:target/rerun.txt",
                 "me.jvt.cucumber.report.PrettyReports:target/cucumber"
